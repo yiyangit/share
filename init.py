cf5321ca2cbe13659af37441fd844703e90c8a70
安逸 <anyiak@outlook.com>
2022-04-15 19:00:03 +0800

    -

--- init.py
+++ init.py
@@ -6,4 +6,4 @@ sys.stdout = open("D:/share/lists.html",encoding='utf-8',mode='w');
 file_name = os.listdir('D:/share/s')
 for name in file_name:
 	print("<a href='./s/"+name+"'>"+name+"</a></br>");
-print
+print("<p>更新于"+time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()));
