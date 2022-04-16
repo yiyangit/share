9fbe4646c5793c067196ad28b4f44d055f4992ae
安逸 <anyiak@outlook.com>
2022-04-16 13:34:12 +0800

    -

--- init.py
+++ init.py
@@ -2,8 +2,8 @@
 import time
 import os
 import sys
-sys.stdout = open("D:/share/lists.html",encoding='utf-8',mode='w');
+sys.stdout = open("D:/share/lists.html",encoding='utf-8',mode='w')
 file_name = os.listdir('D:/share/s')
+print("<p>更新于"+time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+"</p>")
 for name in file_name:
-	print("<a href='./s/"+name+"'>"+name+"</a></br>");
-print("<p>更新于"+time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()));
+	print("<a href='./s/"+name+"'>"+name+"</a></br>")
